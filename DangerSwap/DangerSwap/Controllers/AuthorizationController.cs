84f6c4666129ac92a35ea01c6ccd0a50fcf64d7d
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-02 20:48:27 +0300

    [DZ-5] Create AuthorizationController for managing the login and registration process. Create simple views for registration and authorization.

---
+++ DangerSwap/DangerSwap/Controllers/AuthorizationController.cs
@@ -0,0 +1,74 @@
+﻿using DangerSwap.DbContexts;
+using DangerSwap.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DangerSwap.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly DangerSwapContext _dbContext;
+
+        public AuthorizationController(UserManager<User> userManager, SignInManager<User> signInManager, DangerSwapContext dbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+               var result = await _userManager.CreateAsync(user, user.Password);
+                if (result.Succeeded)
+                {
+                    return View(nameof(Login));
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(nameof(Registration));
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(string email, string password)
+        {
+            try
+            {
+                var user = _dbContext.Users.First(q => q.Email == email);
+                var identityResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("/", nameof(HomeController));
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Username or password is incorrect");
+            }
+            return View(nameof(Login));
+        }
+    }
+}

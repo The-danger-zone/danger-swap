bc7fc980472885945d6766f864938ec46f8ebf67
Vytenis Rimkus <vytenis100@gmail.com>
2022-04-05 12:44:41 +0300

    DZ10 Changed name of r variable to its full name

--- Currency_Scraper/scrapers/scraper.py
+++ Currency_Scraper/scrapers/scraper.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_data():
     url = 'https://www.x-rates.com/table/?from=USD&amount=1'
-    r = requests.get(url)
+    request = requests.get(url)
 
     currency_dict = {}
 
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text, 'html.parser')
-        rate_table = soup.find('table', {'class': 'ratesTable'})
+    if request.status_code == 200:
+        soup = BeautifulSoup(request.text, 'html.parser')
 
+        rate_table = soup.find('table', {'class': 'ratesTable'})
         for currencies in rate_table.find_all('tbody', limit=50):
             rows = currencies.find_all('tr')
             for row in rows:
@@ -20,12 +21,12 @@ def get_data():
     else:
         print("Connection is lost")
 
-    return currency_dict 
+    return currency_dict
 
 def currency_filter(dictionary):
     filtered_dict = {}
     keyCurrencyValues = ['Euro', 'British Pound', 'Chinese Yuan Renminbi']
-    
+
     for key, values in dictionary.items():
         if key in keyCurrencyValues:
             filtered_dict[key] = values

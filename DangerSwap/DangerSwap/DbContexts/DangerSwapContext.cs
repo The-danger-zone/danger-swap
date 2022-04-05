c7a245d46fda6465d5026e3169e0186d18113af8
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-05 23:37:36 +0300

    [DZ-8] Add intermediate modelTransactionCurrencies with migrations

--- DangerSwap/DangerSwap/DbContexts/DangerSwapContext.cs
+++ DangerSwap/DangerSwap/DbContexts/DangerSwapContext.cs
@@ -13,5 +13,6 @@ namespace DangerSwap.DbContexts
         public DbSet<Transaction> Transactions { get; set; } = null!;
         public DbSet<Currency> Currencies { get; set; } = null!;
         public DbSet<Rate> Rate { get; set; } = null!;
+        public DbSet<TransactionCurrency> TransactionCurrencies { get; set; } = null!;
     }
 }
---
+++ DangerSwap/DangerSwap/Migrations/20220405203524_AddTransactionCurrenciesWithManyToMany.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DangerSwap.Migrations
+{
+    public partial class AddTransactionCurrenciesWithManyToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransactionCurrencies",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TransactionId = table.Column<string>(type: "TEXT", nullable: false),
+                    FromId = table.Column<string>(type: "TEXT", nullable: false),
+                    ToId = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionCurrencies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransactionCurrencies_Currencies_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TransactionCurrencies_Currencies_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TransactionCurrencies_Transactions_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionCurrencies_FromId",
+                table: "TransactionCurrencies",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionCurrencies_ToId",
+                table: "TransactionCurrencies",
+                column: "ToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionCurrencies_TransactionId",
+                table: "TransactionCurrencies",
+                column: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionCurrencies");
+        }
+    }
+}

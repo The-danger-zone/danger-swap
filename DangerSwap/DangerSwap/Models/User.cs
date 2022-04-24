ea0f251086a655e0524f7c47ce4e4dfa6883d29e
VytenisRimkus <vytenis100@gmail.com>
2022-04-24 17:36:16 +0300

    DZ-40 Added server side email validation and refined error messages

--- DangerSwap/DangerSwap/Models/User.cs
+++ DangerSwap/DangerSwap/Models/User.cs
@@ -7,16 +7,19 @@ namespace DangerSwap.Models
 {
     public class User : IdentityUser
     {
-        [Required]
+        [Required (ErrorMessage = "The Username field is required")]
         [StringLength(StringLengthConst.defaultString)]
         public override string UserName { get; set; } = string.Empty;
         [Required]
         [StringLength(StringLengthConst.password)]
         [MinLength(StringLengthConst.minPasswordLength)]
         [DataType(DataType.Password)]
+
         public string Password { get; set; } = string.Empty;
         [Required]
         [StringLength(StringLengthConst.email)]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid Email format")]
         public override string Email { get; set; } = string.Empty;
         [Required]
         [StringLength(StringLengthConst.defaultString)]
@@ -24,8 +27,8 @@ namespace DangerSwap.Models
         [Required]
         [StringLength(StringLengthConst.defaultString)]
         public string Nationality { get; set; } = string.Empty;
-        [Required]
-        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage ="The Date of Birth is required")]
+        public DateTime? BirthDate { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         

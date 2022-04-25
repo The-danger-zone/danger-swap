96533f2b5a067ed9cdaf54d922d60a6a49067761
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-26 02:25:50 +0300

    [DZ-6] Add currency repository with all implemented actions

---
+++ DangerSwap/DangerSwap/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DangerSwap.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetEntitiesAsync();
+        Task<T> GetEntity(Guid id);
+        Task UpdateEntity(T entity);
+        Task CreateEntity(T entity);
+        Task DeleteEntity(Guid id);
+        Task<bool> IsExist(Guid id);
+    }
+}

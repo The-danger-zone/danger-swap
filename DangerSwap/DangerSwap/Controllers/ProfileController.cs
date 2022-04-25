185cb68917f906759005dd7f8b67e2bf4ff95c1d
mindaugasGaidys <mindaugas.gaidys@stud.vilniustech.lt>
2022-04-25 20:42:15 +0300

    Added Profile icon with drop down menu, redirects, minor style fixes

---
+++ DangerSwap/DangerSwap/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DangerSwap.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

e6c41a106b093e7a1931fae4b2c485e59c30854c
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-26 02:26:35 +0300

    [DZ-6] Add a method for reading scrapped data

---
+++ DangerSwap/DangerSwap/Models/ScrappedCurrency.cs
@@ -0,0 +1,11 @@
+﻿namespace DangerSwap.Models
+{
+    // Model is used only for Json deserialization 
+    public class ScrappedCurrency
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Change { get; set; } = string.Empty;
+    }
+}
--- DangerSwap/DangerSwap/Services/Scrapper/ScrapperService.cs
+++ DangerSwap/DangerSwap/Services/Scrapper/ScrapperService.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using DangerSwap.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace DangerSwap.Services
 {
@@ -9,6 +11,8 @@ namespace DangerSwap.Services
         private readonly string _installationStatusFile = string.Empty;
         private readonly string _scrapperInstallerFileName = string.Empty;
         private readonly string _scrapperExecutorFileName = string.Empty;
+        private readonly string _cryptoRatesFileName = string.Empty;
+        private readonly string _fiatRatesFileName = string.Empty;
         private readonly Tuple<string, string> _executionPaths;
 
         public ScrapperService(ConfigurationManager configurations)
@@ -21,6 +25,8 @@ namespace DangerSwap.Services
             _installationStatusFile = _installationPath + "status.txt";
             _scrapperInstallerFileName = _configurations["ScrapperInstallerFileName"];
             _scrapperExecutorFileName = _configurations["ScrapperExecutorFileName"];
+            _cryptoRatesFileName = _configurations["CryptoRatesFileName"];
+            _fiatRatesFileName = _configurations["FiatRatesFileName"];
         }
 
         public void RunScrappers()
@@ -67,5 +73,23 @@ namespace DangerSwap.Services
                 return false;
             }
         }
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8603 // Possible null reference return.
+        public IEnumerable<ScrappedCurrency> ReadScrappedCurrencies(bool isFiat)
+        {
+            try
+            {
+                string cryptoRatesJson = File.ReadAllText(isFiat ? _executionPaths.Item2 + _fiatRatesFileName :
+                    _executionPaths.Item1 + _cryptoRatesFileName);
+                IEnumerable<ScrappedCurrency> currencies = JsonConvert.DeserializeObject<IEnumerable<ScrappedCurrency>>(cryptoRatesJson);
+                return currencies;
+            } catch (Exception)
+            {
+                return Enumerable.Empty<ScrappedCurrency>();
+            }
+        }
+#pragma warning restore CS8603 // Possible null reference return.
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
     }
 }

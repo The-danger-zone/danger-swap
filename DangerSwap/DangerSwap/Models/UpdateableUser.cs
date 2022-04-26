598f961b80580436bd9b9a1b42b3b3661bcce780
Ernest Petrovich <ernest.petrovic25@gmail.com>
2022-04-26 10:51:39 +0300

    [DZ-37] Profile management page (#34)

---
+++ DangerSwap/DangerSwap/Models/UpdateableUser.cs
@@ -0,0 +1,30 @@
+﻿using DangerSwap.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace DangerSwap.Models
+{
+    public class UpdateableUser
+    {
+        [Required]
+        [StringLength(StringLengthConst.email)]
+        public string NewEmail { get; set; } = string.Empty;
+        [Required]
+        [StringLength(StringLengthConst.email)]
+        public string ConfirmedNewEmail { get; set; } = string.Empty;
+        [Required]
+        [StringLength(StringLengthConst.password)]
+        [MinLength(StringLengthConst.minPasswordLength)]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; } = string.Empty;
+        [Required]
+        [StringLength(StringLengthConst.password)]
+        [MinLength(StringLengthConst.minPasswordLength)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; } = string.Empty;
+        [Required]
+        [StringLength(StringLengthConst.password)]
+        [MinLength(StringLengthConst.minPasswordLength)]
+        [DataType(DataType.Password)]
+        public string ConfirmedNewPassword { get; set; } = string.Empty;
+    }
+}

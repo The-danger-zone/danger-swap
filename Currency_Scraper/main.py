27d1ec80572d4560ddd1d03cbd3b2c5fa28dded4
VytenisRimkus <vytenis100@gmail.com>
2022-04-26 19:37:16 +0300

    DZ-48 Made code run every 60 seconds

---
+++ Currency_Scraper/main.py
@@ -0,0 +1,19 @@
+from scrapers import currency_scraper
+from filters import filter
+from utilities import file_writer
+import json
+import time
+
+if __name__ == "__main__":
+
+    key_currency_values = ['Euro', 'British Pound', 'Chinese Yuan Renminbi']
+
+    while True:
+        try:
+            currencies = currency_scraper.get_data()
+            filtered_currencies = filter.filter_currency(currencies, key_currency_values)
+            parsed_currencies = json.dumps(filtered_currencies, indent=4)
+            file_writer.write_to_file(parsed_currencies)
+            time.sleep(60)
+        except:
+            raise TypeError

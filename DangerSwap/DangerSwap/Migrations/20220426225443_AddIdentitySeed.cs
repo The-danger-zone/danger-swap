99085c8df61280ccd011b222413a3a0e44a40ae0
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-04-27 03:03:23 +0300

    Add new migration for seeding with the first user.

---
+++ DangerSwap/DangerSwap/Migrations/20220426225443_AddIdentitySeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DangerSwap.Migrations
+{
+    public partial class AddIdentitySeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "BirthDate", "Citizenship", "ConcurrencyStamp", "CreatedAt", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Nationality", "NormalizedEmail", "NormalizedUserName", "Password", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", "b050a2e3-a1c3-4c6c-a187-b82cf6ddfba8", new DateTime(2022, 4, 26, 22, 54, 42, 890, DateTimeKind.Utc).AddTicks(5013), "admin@admin.com", false, false, null, "", "admin@admin.com", "admin", "Admin123#", "AQAAAAEAACcQAAAAEEuLD1dFigkTjmZRnQbEut6oXO0tE8rYl2tavqD8oqZceKgJxp35 + mGldf80qWJrHA ==", null, false, "", false, "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+        }
+    }
+}

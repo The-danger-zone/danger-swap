97b96d750366ae14211804d50a52d46419b23862
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-12-03 21:33:52 +0200

    [DISS] Skip automation tests. Format the code.

--- DangerSwap/DangerSwap.Test/Automated/ConverterTest.cs
+++ DangerSwap/DangerSwap.Test/Automated/ConverterTest.cs
@@ -4,125 +4,125 @@ using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
 using Xunit;
 
-namespace DangerSwap.Test.Automated
+namespace DangerSwap.Test.Automated;
+
+public sealed class ConverterTest
 {
-    public class ConverterTest
+    protected readonly string Url = "https://localhost:7145/";
+    protected readonly IWebDriver _driver = new ChromeDriver();
+
+    [Fact(Skip = "Automation test")]
+    public void LoadApplicationPage()
     {
-        protected readonly string Url = "https://localhost:7145/";
-        protected readonly IWebDriver _driver = new ChromeDriver();
-
-        [SkippableFact]
-        public void LoadApplicationPage()
+        // Arrange
+        string expectedUri = "Authorization/Login?ReturnUrl=%2F";
+        string expectedTitle = "- DangerSwap";
+        using (_driver)
         {
-            // Arrange
-            string expectedUri = "Authorization/Login?ReturnUrl=%2F";
-            string expectedTitle = "- DangerSwap";
-            using (_driver)
-            {
-                // Act
-                _driver.Navigate().GoToUrl(Url);
+            // Act
+            _driver.Navigate().GoToUrl(Url);
 
-                // Assert
-                Assert.Equal(Url + expectedUri, _driver.Url);
-                Assert.Equal(expectedTitle, _driver.Title);
-            }
+            // Assert
+            Assert.Equal(Url + expectedUri, _driver.Url);
+            Assert.Equal(expectedTitle, _driver.Title);
         }
+    }
 
-        [SkippableFact]
-        public void Logout_AfterUserSuccessfullyLoggedIn()
+    [Fact(Skip = "Automation test")]
+    public void Logout_AfterUserSuccessfullyLoggedIn()
+    {
+        // Arrange
+        using (_driver)
         {
-            // Arrange
-            using (_driver)
-            {
-                string expectedUri = "Authorization/Login";
-                _driver.Manage().Window.Maximize();
-                _driver.Navigate().GoToUrl(Url);
-                var passwordInput = _driver.FindElement(By.Id("Password"));
-                var emailInput = _driver.FindElement(By.Id("Email"));
-                var loginButton = _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div/form/div/div[5]/input[1]"));
+            string expectedUri = "Authorization/Login";
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(Url);
+            var passwordInput = _driver.FindElement(By.Id("Password"));
+            var emailInput = _driver.FindElement(By.Id("Email"));
+            var loginButton = _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div/form/div/div[5]/input[1]"));
 
-                // Act
-                emailInput.SendKeys("admin@admin.com");
-                passwordInput.SendKeys("Admin123#");
-                loginButton.Click();
-                var dropdownMenuButton = _driver.FindElement(By.Id("dropdownMenuButton"));
-                dropdownMenuButton.Click();
-                var logoutButton = _driver.FindElement(By.XPath("/html/body/header/nav/div/div[2]/div/div/form/button"));
-                logoutButton.Click();
+            // Act
+            emailInput.SendKeys("admin@admin.com");
+            passwordInput.SendKeys("Admin123#");
+            loginButton.Click();
+            var dropdownMenuButton = _driver.FindElement(By.Id("dropdownMenuButton"));
+            dropdownMenuButton.Click();
+            var logoutButton = _driver.FindElement(By.XPath("/html/body/header/nav/div/div[2]/div/div/form/button"));
+            logoutButton.Click();
 
 
-                // Assert
-                Assert.Equal(Url + expectedUri, _driver.Url);
-            }
+            // Assert
+            Assert.Equal(Url + expectedUri, _driver.Url);
         }
+    }
 
-        [SkippableTheory]
-        [MemberData(nameof(ConverterData))]
-        public void ConvertCurrency(int firstOption, int secondOption, int amount, bool isExpectedToBeConverted)
+    [Theory(Skip = "Automation test")]
+    [MemberData(nameof(ConverterData))]
+    public void ConvertCurrency(int firstOption, int secondOption, int amount, bool isExpectedToBeConverted)
+    {
+        // Arrange
+        using (_driver)
         {
-            // Arrange
-            using (_driver)
-            {
-                _driver.Manage().Window.Maximize();
-                _driver.Navigate().GoToUrl(Url);
-                var passwordInput = _driver.FindElement(By.Id("Password"));
-                var emailInput = _driver.FindElement(By.Id("Email"));
-                var loginButton = _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div/form/div/div[5]/input[1]"));
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(Url);
+            var passwordInput = _driver.FindElement(By.Id("Password"));
+            var emailInput = _driver.FindElement(By.Id("Email"));
+            var loginButton = _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div/form/div/div[5]/input[1]"));
 
-                // Act
-                emailInput.SendKeys("admin@admin.com");
-                passwordInput.SendKeys("Admin123#");
-                loginButton.Click();
-                var fiatCurrencyDropDown = _driver.FindElement(By.Id("TransactionCurrency_FromId"));
-                var cryptoCurrencyDropDown = _driver.FindElement(By.Id("TransactionCurrency_ToId"));
-                var amountInput = _driver.FindElement(By.Id("Amount"));
-                var convertButton = _driver.FindElement(By.XPath("/html/body/div/main/form/div[3]/input"));
-                SelectElement fiatCurrencySelect = new SelectElement(fiatCurrencyDropDown);
-                fiatCurrencySelect.Options[firstOption].Click();
-                SelectElement cryptoCurrencySelect = new SelectElement(cryptoCurrencyDropDown);
-                cryptoCurrencySelect.Options[secondOption].Click();
-                amountInput.SendKeys(amount.ToString());
-                convertButton.Click();
-                var equivalent = _driver.FindElement(By.XPath("/html/body/div/main/form/div[2]/input[2]"));
-                bool isConverted = equivalent.GetAttribute("Value") != "0.";
+            // Act
+            emailInput.SendKeys("admin@admin.com");
+            passwordInput.SendKeys("Admin123#");
+            loginButton.Click();
+            var fiatCurrencyDropDown = _driver.FindElement(By.Id("TransactionCurrency_FromId"));
+            var cryptoCurrencyDropDown = _driver.FindElement(By.Id("TransactionCurrency_ToId"));
+            var amountInput = _driver.FindElement(By.Id("Amount"));
+            var convertButton = _driver.FindElement(By.XPath("/html/body/div/main/form/div[3]/input"));
+            SelectElement fiatCurrencySelect = new SelectElement(fiatCurrencyDropDown);
+            fiatCurrencySelect.Options[firstOption].Click();
+            SelectElement cryptoCurrencySelect = new SelectElement(cryptoCurrencyDropDown);
+            cryptoCurrencySelect.Options[secondOption].Click();
+            amountInput.SendKeys(amount.ToString());
+            convertButton.Click();
+            var equivalent = _driver.FindElement(By.XPath("/html/body/div/main/form/div[2]/input[2]"));
+            bool isConverted = equivalent.GetAttribute("Value") != "0.";
 
-                // Assert
-                Assert.Equal(isConverted, isExpectedToBeConverted);
-            }
+            // Assert
+            Assert.Equal(isConverted, isExpectedToBeConverted);
         }
+    }
 
-        private static IEnumerable<object[]> ConverterData()
-        {
-            yield return new object[] {
+    private static IEnumerable<object[]> ConverterData()
+    {
+        yield return new object[] {
                 1,
                 1,
                 40000,
                 true
             };
-            yield return new object[] {
+        yield return new object[] {
                 0,
                 1,
                 40000,
                 false,
             };
-            yield return new object[] {
+        yield return new object[] {
                 1,
                 0,
                 40000,
                 false,
             };
-            yield return new object[] {
+        yield return new object[] {
                 0,
                 0,
                 40000,
                 false,
             };
-            yield return new object[] {
+        yield return new object[] {
                 0,
                 0,
                 0,
                 false,
             };
-        }
     }
 }
+

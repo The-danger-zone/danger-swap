a1b55075b146dda00764ab0d29273f89ff197043
Ernie25 <ernest.petrovic25@gmail.com>
2022-12-21 01:54:47 +0200

    Add missing migrations and files

---
+++ DangerSwap/DangerSwap/Interfaces/ICapitalService.cs
@@ -0,0 +1,14 @@
+﻿using DangerSwap.Models;
+
+namespace DangerSwap.Interfaces;
+
+public interface ICapitalService
+{
+    Task DepositCapital(Capital capital, User user);
+
+    Task<IEnumerable<Capital>> GetCapitalsAsync(Guid userId);
+    
+    Task<bool> WithdrawCapital(string capitalId, decimal amount);
+
+    Task<bool> ConvertCapital(string sourceCurrencyId, string targetCurrencyId, decimal amount, decimal convertedEquivalent, User user);
+}
---
+++ DangerSwap/DangerSwap/Models/ViewModel/TransactionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DangerSwap.Models.ViewModel;
+
+public sealed class TransactionViewModel
+{
+    public Transaction Transaction { get; set; } = null!;
+
+    public bool IsCapitalUsed { get; set; }
+}
